8de147721be5712bddf163677b0cb2a8636c7a7c
Stanislau Kavaliou <kavalioustanislau@gmail.com>
2016-12-31 00:35:09 +0300

    Added task for clean old values.

--- currencies/tasks.py
+++ currencies/tasks.py
@@ -1,9 +1,11 @@
 import json
-from datetime import timedelta
+from datetime import timedelta, datetime
 
+from celery.schedules import crontab
 from celery.task import periodic_task, task
+from django.db import models
 
-from currencies.models import CurrencyPair
+from currencies.models import CurrencyPair, CurrencyPairValue
 from dealing_center_settings.models import Setting
 from .currency_generator import generator
 from .currency_history_generator import generate_history_for_currency_pair
@@ -27,3 +29,16 @@ def generate_history():
 @task
 def history_for_currency_pair(pair):
     generate_history_for_currency_pair(pair)
+
+
+@periodic_task(run_every=crontab(hour='0', minute='5'))
+def clean_currency_pair_values():
+    for currency_pair in CurrencyPair.objects.all():
+        yesterday = datetime.now().date() - timedelta(days=1)
+        CurrencyPairValue.objects \
+            .filter(currency_pair=currency_pair) \
+            .filter(creation_time__date__lt=yesterday) \
+            .annotate(start_value_count=models.Count('start_value'),
+                      end_value_count=models.Count('end_value')) \
+            .filter(start_value_count=0, end_value_count=0) \
+            .delete()
